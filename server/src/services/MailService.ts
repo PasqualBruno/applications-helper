770d80fc3a53270a486f30d68813928741e4a5a9
PasqualBruno <pasqualbruno97@gmail.com>
2026-03-24 15:07:51 -0300

    fix: adjust and correction for heroku deploy

---
+++ server/src/services/MailService.ts
@@ -0,0 +1,36 @@
+import nodemailer from "nodemailer";
+import { generateJobEmail } from "../templates/jobTemplate.js";
+import type { ISendEmailRequest } from "../types/mail.js";
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+export const sendJobEmail = async ({
+  jobName,
+  recipientEmail,
+}: ISendEmailRequest): Promise<void> => {
+  const template = generateJobEmail(jobName);
+
+  await transporter.sendMail({
+    from: `Bruno Pasqual <${process.env.EMAIL_USER}>`,
+    to: recipientEmail,
+    subject: `Currículo ${jobName} - Bruno Pasqual`,
+    text: template,
+    attachments: [
+      {
+        filename: "curriculo - Bruno Pasqual.pdf",
+        path: "./curriculo - Bruno Pasqual.pdf",
+      },
+    ],
+  });
+};
